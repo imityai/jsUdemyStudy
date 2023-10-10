cb44c0fd045815fa222e0eae2db9204ddb501d79
imitya <mitya.mitya.dima@gmail.com>
2023-10-10 13:25:49 +0400

    lesson if

--- script.js
+++ script.js
@@ -17,3 +17,37 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
+
+//урок про условия
+
+const num = 50;
+
+if (num < 49) {
+	console.log('Error');
+} else if (num > 100) {
+	console.log('too much');
+} else {
+	console.log('Ok!')
+}
+
+//1.2 Условный (тернарный) оператор
+
+(num === 50) ? console.log('Ok!') : console.log('Erorr');
+
+//1.3 Switch
+
+switch (num) {
+	case 49:
+		console.log('no');
+		break;
+	case 100:
+		console.log('not again');
+		break;
+	case 50:
+		console.log('yeap');
+		break;
+	default:
+		console.log('если ничего не выолнено выше');
+		break;
+}
